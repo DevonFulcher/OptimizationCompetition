5ac780a07264e27876e1a3f64477f60ee9d65efb
DevonFulcher <devonfulcher3@gmail.com>
2018-09-05 14:31:23 -0500

    refactor SimpleTest into its own file.
    last commit prior to adding constraints.

---
+++ SimpleTestCompetition.sol
@@ -0,0 +1,13 @@
+import "/OptimzationCompetition.sol";
+
+contract SimpleTestCompetition is OptimizationCompetition(500, true) {
+    /**
+     * This is an incredibly simple example of an optimization problem
+     */
+    function objectiveFunction(int[] parameters) private pure returns(int y) {
+        require(parameters.length == 1);
+        //y = (x - 1)^2
+        //optimum: x = 1
+        return (parameters[0] - 1) * (parameters[0] - 1);
+    }
+}
